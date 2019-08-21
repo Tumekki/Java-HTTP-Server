13f81b5319e55dfade6436a44c19d58178805089
Tuukka <tospartanen@gmail.com>
2019-08-21 09:31:26 +0300

    Initial commit

---
+++ JavaServerHTTP/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+	final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080...");
+        while(true){
+            final Socket client = server.accept();
+        }
+    }
+}
